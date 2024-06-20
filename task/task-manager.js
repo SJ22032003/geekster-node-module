420a08ae363d1d7d18a1dde98a59121fbaccdfa4
SJ22032003 <jaryalshaurya2203@gmail.com>
2024-06-20 22:36:28 +0530

    Add task management functionality to task-manager.js

--- task/task-manager.js
+++ task/task-manager.js
@@ -28,25 +28,80 @@ const saveMyTasks = (tasks) => {
 }
 
 const addTask = (task) => {
-  const tasks = getMyTasks();
+  const tasks = getMyTasks(); // [{}, {}]
   tasks.push({ description: task, completed: false });
   saveMyTasks(tasks);
   console.info("Task added successfully");
 }
 
 // listing
-// updating
+const listTasks = () => {
+  const tasks = getMyTasks(); 
+  tasks.forEach((task, index) => {
+    console.log(`${index + 1}. ${task.description} - [${task.completed ? "x" : " "}]`)
+  }) 
+}
+
+// complete a task
+const completeTask = (taskNo) => {
+  const tasks = getMyTasks(); // Array<{description: string, completed: boolean}>
+  if(tasks[taskNo - 1]) {
+    tasks[taskNo - 1].completed = true;
+    saveMyTasks(tasks);
+    console.info("Task completed successfully");
+  } else {
+    console.warn("Invalid task number");
+  }
+  return;
+}
+
 // deleting
+const deleteTask = (taskNo) => {
+  const tasks = getMyTasks();
+  if(tasks[taskNo - 1]) {
+    const filteredTask = tasks.filter((task, index) => index !== taskNo - 1);
+    saveMyTasks(filteredTask);
+    console.info("Task deleted successfully");
+  } else {
+    console.warn("Invalid task number");
+  }
+  return;
+
+}
 
 function todoManager() {
-  rl.question("What would you like to do?\n1. Add a task\n", (answer) => {
+  rl.question(`What would you like to do?
+  1. Add a task
+  2. List all tasks
+  3. Mark task as completed
+  4. Delete Task
+  5. Exit
+  `, (answer) => {
     switch(answer) {
       case "1":
         rl.question("Enter your task: ", (task) => {
-          console.log(`Adding task: ${task}`);
           addTask(task);
           todoManager();
         })
+        break;
+      case "2":
+        listTasks();
+        todoManager();
+        break;
+      case "3":
+        rl.question("Enter the task number you want to complete: ", (taskNo) => {
+          completeTask(+taskNo); // string number into number
+          todoManager();
+        });
+        break
+      case "4":
+        rl.question("Enter the task number you want to delete: ", (taskNo) => {
+          deleteTask(+taskNo);
+          todoManager();
+        });
+        break;
+      case "5":
+        rl.close();
         break;
       default:
         console.log("Invalid option");
