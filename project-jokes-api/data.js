ec1b26322c2676fe5f412bc0c0dc65d81b8f6f83
SJ22032003 <jaryalshaurya2203@gmail.com>
2024-06-28 22:47:08 +0530

    chore: Add project-jokes-api with server.js, data.js, and package.json

---
+++ project-jokes-api/data.js
@@ -0,0 +1,28 @@
+const jokes = [
+  {
+    id: 0,
+    joke: " What do kids play when their mom is using the phone? Bored games."
+  },
+  {
+    id: 1,
+    joke: " What do kids play when their mom is using the phone? Bored games."
+  },
+  {
+    id: 2,
+    joke: "Why did the tomato turn red? Because it saw the salad dressing!"
+  },
+  {
+    id: 3,
+    joke: "What do you call a fake noodle? An impasta."
+  },
+  {
+    id: 4,
+    joke: "Why did the scarecrow win an award? Because he was outstanding in his field."
+  },
+  {
+    id: 5,
+    joke: "What do you call a bear with no teeth? A gummy bear."
+  }
+];
+
+module.exports = jokes;
